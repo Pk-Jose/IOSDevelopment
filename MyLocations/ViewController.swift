6b1a2cbfeb1633b57824b2b0cbfddae93a276f31
Jose Sosa <josesosa2001@gmail.com>
2022-09-06 08:21:00 -0400

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Jose Sosa on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
